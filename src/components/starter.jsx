01000b438e8f81d09597ba81c2d65787e308d7e3
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-10-02 13:21:13 +0400

    Added some styles

--- src/components/starter.jsx
+++ src/components/starter.jsx
@@ -1,22 +1,24 @@
-export default function starter({ notifications, setNotifications }) {
+export default function Starter({ notifications, setNotifications }) {
   const counter = notifications.filter(
     (notification) => !notification.isRead
   ).length;
 
   function markAllAsRead() {
-    const newNotifications = notifications.map((notification) => {
-      return { ...notification, isRead: true };
-    });
+    const newNotifications = notifications.map((notification) => ({
+      ...notification,
+      isRead: true,
+    }));
     setNotifications(newNotifications);
   }
 
   return (
-    <header>
-      {" "}
-      <section>
-        <h2>Notifications {counter}</h2>
-        <p onClick={markAllAsRead}>Mark all as read</p>
-      </section>
+    <header className="w-full flex justify-center">
+      <div className="w-[343px] my-[24px]">
+        <section className="flex items-center justify-between">
+          <h2 className="text-[#1c202b]">Notifications {counter}</h2>
+          <span onClick={markAllAsRead}>Mark all as read</span>
+        </section>
+      </div>
     </header>
   );
 }
