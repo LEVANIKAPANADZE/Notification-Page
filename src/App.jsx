9d974afeac8a76a47363b65a2d890c1b72158a1f
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-10-02 12:54:13 +0400

    Installed tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import data from "./data.json";
-import Notification from "./components/notification/notification";
-import Starter from "./components/starter/starter";
+import Notification from "./components/notification";
+import Starter from "./components/starter";
 
 function App() {
   const [notifications, setNotifications] = useState(data);
